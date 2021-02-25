57e56959f22cbb56b381c97e1540ef056a516bb2
Milad.A <m.abooali@hotmail.com>
2021-02-25 10:58:35 +0300

    isEnvOS

--- src/php/env.php
+++ src/php/env.php
@@ -6,7 +6,7 @@
  * @param string|null $os 'Windows', 'BSD', 'Darwin', 'Solaris', 'Linux' or 'Unknown'.
  * @return bool
  */
-function isEnvOS ($os=null) {
-    if ($os) return (strtolower(PHP_OS_FAMILY) === strtolower($os));
+function isEnvOS($os=null): bool|string {
+    if($os) return(strtolower(PHP_OS_FAMILY) === strtolower($os));
     return PHP_OS_FAMILY;
 }

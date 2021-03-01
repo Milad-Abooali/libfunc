75802e7031694af50c171efec5d30199e56f15f9
Milad.A <m.abooali@hotmail.com>
2021-03-01 10:11:15 +0300

    Trait

--- src/php/env.php
+++ src/php/env.php
@@ -1,13 +1,22 @@
 <?php
 
 /**
- * Check if Os is ?
- * PHP 8.0
- * Pass null to return PHP_OS_FAMILY
- * @param string|null $os 'Windows', 'BSD', 'Darwin', 'Solaris', 'Linux' or 'Unknown'.
- * @return bool
+ * Trait env
+* PHP 8.0
  */
-function isEnvOS($os=null): bool|string {
-    if($os) return(strtolower(PHP_OS_FAMILY) === strtolower($os));
-    return PHP_OS_FAMILY;
+trait env
+{
+
+    /**
+     * Check if Os is ?
+     * PHP 8.0
+     * Pass null to return PHP_OS_FAMILY
+     * @param string|null $os 'Windows', 'BSD', 'Darwin', 'Solaris', 'Linux' or 'Unknown'.
+     * @return bool
+     */
+    public function isEnvOS($os=null): bool|string {
+        if($os) return(strtolower(PHP_OS_FAMILY) === strtolower($os));
+        return PHP_OS_FAMILY;
+    }
+
 }
